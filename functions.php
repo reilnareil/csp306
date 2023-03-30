6d28b4457d20815d082dcffdf42ced59265edd03
Reilan Cad <reilancadubla@gmail.com>
2023-03-30 12:26:53 +0800

    1st push

---
+++ functions.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+   
+   <head>
+      <title>Writing PHP Function with Parameters</title>
+   </head>
+   <body>
+   
+      <?php
+         function addFunction($num1, $num2) {
+            $sum = $num1 + $num2;
+            echo "Sum of the two numbers is : $sum";
+         }
+         $x = 10;
+         $y = 20;
+         addFunction($x, $y);
+      ?>
+      
+   </body>
+</html>
